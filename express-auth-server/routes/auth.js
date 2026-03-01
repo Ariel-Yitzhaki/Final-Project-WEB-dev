b187e732ff3d45e8723ee2a2487384243129446b
Ariel Yitzhaki <arielyitzhaki900@gmail.com>
2026-03-01 11:32:55 +0200

    Changed settokencookie to send cross-site

--- express-auth-server/routes/auth.js
+++ express-auth-server/routes/auth.js
@@ -18,7 +18,7 @@ function setTokenCookie(res, token) {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: 'lax',
+        sameSite: 'none',
         maxAge: 24 * 60 * 60 * 1000, // 1 day
     });
 }
