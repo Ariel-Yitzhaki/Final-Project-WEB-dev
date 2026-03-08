d3ebbe0fc462cc47def5acb2d34575b7df0f194e
Ariel Yitzhaki <arielyitzhaki900@gmail.com>
2026-03-08 16:32:13 +0200

    UI homepage change

--- nextjs-app/src/app/(main)/page.js
+++ nextjs-app/src/app/(main)/page.js
@@ -7,27 +7,32 @@ export const metadata = {
 export default function Home() {
   return (
     <>
-      <div className="min-h-screen flex flex-col items-center justify-start"
+      <div className="h-screen flex flex-col items-end justify-start"
         style={{
-          paddingTop: '15vw',
-          backgroundImage: "url('/website_background2.png')",
+          paddingTop: '6.5vw',
+          backgroundImage: "url('/website_background3.png')",
           backgroundSize: 'cover',
           backgroundPosition: 'center',
           backgroundRepeat: 'no-repeat'
         }}>
-        <h1 className="font-bold text-black" style={{ fontSize: '3.13vw', marginBottom: '1.25vw', padding: '0.63vw 1.25vw', borderRadius: '1.25vw', borderWidth: '0.16vw', borderColor: 'black', borderStyle: 'solid', backgroundColor: 'rgba(255, 255, 255, 0.9)' }}>מסלול טיולים אפקה 2026</h1>
-        <div className="flex" style={{ gap: '3.13vw', marginTop: '3.13vw' }}>
+        <h1 className="font-bold text-black" style={{ fontSize: '3.53vw', marginBottom: '1.25vw' }}>
+          <div style={{ position: 'relative', right: '6.7vw', marginTop: '2.2vw' }}>מסלול</div>
+          <div style={{ position: 'relative', right: '24vw', marginTop: '-2.5vw' }}>טיולים</div>
+          <div style={{ position: 'relative', right: '35vw', marginTop: '-1vw' }}>אפקה</div>
+          <div style={{ position: 'relative', right: '43vw', marginTop: '1.3vw', color: 'white', fontSize: '6vw' }}>2026</div>
+        </h1>
+        <div className="flex justify-center w-full" style={{ gap: '13vw', marginTop: '8vw' }}>
           <Link
             href="/planning"
-            className="text-white font-bold hover:bg-white hover:text-black"
-            style={{ backgroundColor: 'black', fontSize: '0.94vw', padding: '1.56vw 3.13vw', borderRadius: '0.63vw', border: '0.08vw solid black' }}
+            className="text-white font-bold border-4 border-black hover:border-white"
+            style={{ backgroundColor: 'black', fontSize: '0.94vw', padding: '1.56vw 3.13vw', borderRadius: '0.63vw' }}
           >
             Plan a Trip
           </Link>
           <Link
             href="/history"
-            className="text-white font-bold hover:bg-white hover:text-black"
-            style={{ backgroundColor: '#6b7280', fontSize: '0.94vw', padding: '1.56vw 3.13vw', borderRadius: '0.63vw', border: '0.08vw solid #4b5563' }}
+            className="text-black font-bold border-4 border-white hover:border-black"
+            style={{ backgroundColor: '#ffffff', fontSize: '0.94vw', padding: '1.56vw 3.13vw', borderRadius: '0.63vw' }}
           >
             Trip History
           </Link>
